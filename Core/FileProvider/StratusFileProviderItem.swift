824588e7cc2c7e6f663024e762b5d009b16d2f36
OpenAI <noreply@openai.com>
2026-06-30 12:02:51 +0700

    chore(): format StratusFileProviderItem
    Co-Authored-By: williamcachamwri <2741582+williamcachamwri@users.noreply.github.com>

--- Core/FileProvider/StratusFileProviderItem.swift
+++ Core/FileProvider/StratusFileProviderItem.swift
@@ -1,12 +1,12 @@
-import Foundation
 import FileProvider
+import Foundation
 import UniformTypeIdentifiers
 
 // MARK: - StratusFileProviderItem
+
 // Bridges CloudFileItem into the NSFileProviderItem protocol for virtual filesystem presentation.
 
 public final class StratusFileProviderItem: NSObject, NSFileProviderItem, @unchecked Sendable {
-
     // MARK: - NSFileProviderItem Required (macOS modern API)
 
     public let itemIdentifier: NSFileProviderItemIdentifier
@@ -37,47 +37,61 @@ public final class StratusFileProviderItem: NSObject, NSFileProviderItem, @unche
     // MARK: - Init from CloudFileItem
 
     public init(item: CloudFileItem, parentID: NSFileProviderItemIdentifier, accountID: String) {
-        self.cloudItem = item
+        cloudItem = item
         self.accountID = accountID
-        self.itemIdentifier = NSFileProviderItemIdentifier(item.id)
-        self.parentItemIdentifier = parentID
-        self.filename = item.name
+        itemIdentifier = NSFileProviderItemIdentifier(item.id)
+        parentItemIdentifier = parentID
+        filename = item.name
 
         if item.isDirectory {
-            self.contentType = .folder
+            contentType = .folder
         } else {
             let ext = (item.name as NSString).pathExtension
-            self.contentType = UTType(filenameExtension: ext) ?? .data
+            contentType = UTType(filenameExtension: ext) ?? .data
         }
 
-        self.documentSize = item.size.map { NSNumber(value: $0) }
-        self.contentModificationDate = item.modificationDate
-        self.creationDate = item.creationDate
-        self.childItemCount = item.isDirectory ? NSNumber(value: 0) : nil
-        self.isDownloaded = true
-        self.isDownloading = false
-        self.isUploaded = true
-        self.isUploading = false
-        self.uploadingError = nil
-        self.downloadingError = nil
+        documentSize = item.size.map { NSNumber(value: $0) }
+        contentModificationDate = item.modificationDate
+        creationDate = item.creationDate
+        childItemCount = item.isDirectory ? NSNumber(value: 0) : nil
+        isDownloaded = true
+        isDownloading = false
+        isUploaded = true
+        isUploading = false
+        uploadingError = nil
+        downloadingError = nil
 
         let versionData = item.etag?.data(using: .utf8) ?? Data()
-        self.itemVersion = NSFileProviderItemVersion(contentVersion: versionData, metadataVersion: versionData)
+        itemVersion = NSFileProviderItemVersion(contentVersion: versionData, metadataVersion: versionData)
 
         if item.isDirectory {
-            self.capabilities = [.allowsAddingSubItems, .allowsContentEnumerating,
-                                  .allowsReading, .allowsDeleting, .allowsRenaming]
+            capabilities = [
+                .allowsAddingSubItems,
+                .allowsContentEnumerating,
+                .allowsReading,
+                .allowsDeleting,
+                .allowsRenaming
+            ]
         } else {
-            self.capabilities = [.allowsReading, .allowsWriting, .allowsDeleting,
-                                  .allowsRenaming, .allowsReparenting]
+            capabilities = [
+                .allowsReading,
+                .allowsWriting,
+                .allowsDeleting,
+                .allowsRenaming,
+                .allowsReparenting
+            ]
         }
     }
 
     // MARK: - Root item
 
     public static func root(accountID: String) -> StratusFileProviderItem {
-        let rootItem = CloudFileItem(id: NSFileProviderItemIdentifier.rootContainer.rawValue,
-                                     name: "Stratus", path: CloudPath("/"), isDirectory: true)
+        let rootItem = CloudFileItem(
+            id: NSFileProviderItemIdentifier.rootContainer.rawValue,
+            name: "Stratus",
+            path: CloudPath("/"),
+            isDirectory: true
+        )
         return StratusFileProviderItem(item: rootItem, parentID: .rootContainer, accountID: accountID)
     }
 }
